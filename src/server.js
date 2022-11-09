e8d4ad4d9c7b6ebe608ff3d02e513806c2e1c727
ericlesbrum <ericlespbrum@gmail.com>
2022-11-09 16:20:11 -0300

    estrutura inicial

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//const path=require('path');
+
+//const db=require('./database');
+const routes = require('./routes/routes');
+
+const app = express();
+
+//db.connect()
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => { console.log(`Server is listening on port ${port}`) });
