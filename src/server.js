1629e25b6866cfcae1f1e196dd41243d9cdb5784
ericlesbrum <ericlespbrum@gmail.com>
2022-11-11 11:01:44 -0300

    adicionando middleware de cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
-//const path=require('path');
+const cors = require('cors');
 
-const db=require('./database/db');
+const db = require('./database/db');
 const routes = require('./routes/routes');
 
 const app = express();
 
 db.connect();
+
+const allowedOrigins =[
+    'http://127.0.0.1:5500'
+]
+
+app.use(cors({
+    origin: function(origin,callback){
+        let allowed=true;
+        if(!origin)
+            allowed=true;
+        if(!allowedOrigins.includes(origin))
+            allowed=false;
+        callback(null,allowed);
+    }
+}));
 
 app.use(express.json());
 
